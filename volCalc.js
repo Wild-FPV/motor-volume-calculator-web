15b1fe45520c30276f785c830b6342b9870c53fa
Wild-FPV <102388595+Wild-FPV@users.noreply.github.com>
2023-12-23 12:57:34 -0500

    Fix volume calculation

--- volCalc.js
+++ volCalc.js
@@ -11,7 +11,7 @@ function calculate(){
     let statorHeight = document.getElementById("motor_height").value;
 
     if(statorHeight > 0 && statorWidth > 0){
-    let volume = statorWidth*statorWidth*(Math.PI)*statorHeight
+    let volume = Math.pow((statorWidth/2),2)*(Math.PI)*statorHeight
     document.getElementById("resulttext").innerHTML = "Result : " + Math.round(volume*10)/10;
     }
     else{
